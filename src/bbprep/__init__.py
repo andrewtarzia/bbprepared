a16e72f41e29d74671271da630b74913cb1fe9d6
Andrew Tarzia <andrew.tarzia@gmail.com>
2023-08-04 12:35:10 +0100

    Start minimise angle process.

--- src/bbprep/__init__.py
+++ src/bbprep/__init__.py
@@ -1,10 +1,15 @@
-from bbprep import generators
+from bbprep import generators, selectors
 from bbprep._internal.ensemble.ensemble import Conformer, Ensemble
+from bbprep._internal.processes.angle import MinimiseAngle
 from bbprep._internal.processes.planarfy import Planarfy
+from bbprep._internal.processes.process import Process
 
 __all__ = [
     "generators",
+    "selectors",
     "Ensemble",
     "Conformer",
+    "Process",
     "Planarfy",
+    "MinimiseAngle",
 ]
---
+++ src/bbprep/_internal/processes/angle.py
@@ -0,0 +1,32 @@
+import stk
+import stko
+
+from bbprep._internal.ensemble.ensemble import Conformer
+
+from .process import Process
+
+
+class MinimiseAngle(Process):
+    """
+    Get the molecule with the min/max of a target angle.
+
+    """
+
+    def _run_process(
+        self,
+        conformer: Conformer,
+        conformer_id: int,
+    ) -> float:
+        key = stk.Smiles().get_key(conformer.molecule) + f"__{conformer_id}"
+        if key in self._data:
+            return self._data[key]
+
+        pc = stko.PlanarityCalculator()
+        pc_results = pc.get_results(
+            conformer.molecule,
+            plane_atom_ids=self._selector(conformer.molecule),
+            deviation_atom_ids=self._selector(conformer.molecule),
+        )
+        value = pc_results.get_planarity_parameter()
+        self._save_to_data(conformer, conformer_id, value)
+        return self._data[key]
