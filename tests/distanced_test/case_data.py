9b8caa7098323466fb71c48c64433f349a3b0741
andrewtarzia <andrew.tarzia@gmail.com>
2024-03-20 10:06:28 +0100

    Add random FG selection.

--- tests/distanced_test/case_data.py
+++ tests/distanced_test/case_data.py
@@ -10,10 +10,12 @@ class CaseData:
         desired_functional_groups: int,
         closest_ids: tuple[int, int],
         furthest_ids: tuple[int, int],
+        random_ids: dict[int, tuple[int, int]],
         name: str,
     ) -> None:
         self.molecule = molecule
         self.desired_functional_groups = desired_functional_groups
         self.closest_ids = closest_ids
         self.furthest_ids = furthest_ids
+        self.random_ids = random_ids
         self.name = name
