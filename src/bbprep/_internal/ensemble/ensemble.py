2216f6a28001f6eb652a0314ccd3ababc8b69250
andrewtarzia <andrew.tarzia@gmail.com>
2024-12-15 17:25:56 +0100

    Add to ensemble interface.

--- src/bbprep/_internal/ensemble/ensemble.py
+++ src/bbprep/_internal/ensemble/ensemble.py
@@ -1,9 +1,12 @@
 """Module for ensemble class."""
 
+import typing
 from collections import abc
 from dataclasses import dataclass
 
 import stk
+
+from .calculators import EnergyCalculator, Optimiser
 
 
 @dataclass
@@ -39,11 +42,56 @@ class Ensemble:
         for conf_id in self._conformers:
             yield self._conformers[conf_id]
 
+    def get_conformers(self) -> dict[int, Conformer]:
+        return self._conformers
+
+    def get_conformer(self, idx: int) -> Conformer:
+        try:
+            return self._conformers[idx]
+        except KeyError as e:
+            msg = f"Conformer with ID: {idx} not in ensemble."
+            raise KeyError(msg) from e
+
     def get_base_molecule(self) -> stk.BuildingBlock:
         return self._base_molecule
 
     def get_molecule_num_atoms(self) -> int:
         return self._molecule_num_atoms
+
+    def optimise_conformers(self, optimiser: Optimiser) -> typing.Self:
+        """Get a new ensemble with optimised conformers."""
+        new_ensemble = Ensemble(base_molecule=self._base_molecule)
+        for conformer in self.yield_conformers():
+            new_ensemble.add_conformer(
+                conformer=Conformer(
+                    molecule=optimiser.function(conformer.molecule),
+                    conformer_id=conformer.conformer_id,
+                    source=conformer.source + f":{optimiser.name}",
+                    permutation=None,
+                )
+            )
+        return new_ensemble
+
+    def get_lowest_energy_conformer(
+        self, calculator: EnergyCalculator
+    ) -> Conformer:
+        """Get the lowest energy conformer based on a calculator.
+
+        The calculator must be a callable that returns a value.
+        """
+        lowest_energy_conformer: Conformer
+        lowest_energy = float("inf")
+        for conformer in self.yield_conformers():
+            energy = calculator.function(conformer.molecule)
+            if energy < lowest_energy:
+                lowest_energy = energy
+                lowest_energy_conformer = Conformer(
+                    molecule=conformer.molecule,
+                    conformer_id=conformer.conformer_id,
+                    source=conformer.source,
+                    permutation=None,
+                )
+        return lowest_energy_conformer
 
     def __str__(self) -> str:
         return (
